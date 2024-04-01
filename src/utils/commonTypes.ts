c44ed3fe2ee5426478643db7ad9ae766eb93771f
link2salman <link2s.razzaq@gmail.com>
2024-04-01 15:16:20 +0500

    improving style

--- src/utils/commonTypes.ts
+++ src/utils/commonTypes.ts
@@ -1,5 +1,14 @@
-import React, { KeyboardEvent } from "react"
+import React, { KeyboardEvent } from "react";
 
-export type ColorsTypes = 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success' 
-export type OnClickTypes = (event:React.MouseEvent | KeyboardEvent<HTMLButtonElement>)=>void
-export type SizeTypes = 'sm' | 'md' | 'lg'
+export type ColorsTypes =
+  | "primary"
+  | "secondary"
+  | "error"
+  | "warning"
+  | "info"
+  | "success"
+  | "common";
+export type OnClickTypes = (
+  event: React.MouseEvent | KeyboardEvent<HTMLButtonElement>
+) => void;
+export type SizeTypes = "sm" | "md" | "lg";
