1230f21e6e1f5dc6e16d8ac92f33885c4f402ce3
link2salman <link2s.razzaq@gmail.com>
2024-09-17 11:17:28 +0500

    code updated

--- src/container/sites/components/RecommendationsSuspense/RecommendationSuspense.tsx
+++ src/container/sites/components/RecommendationsSuspense/RecommendationSuspense.tsx
@@ -2,7 +2,7 @@ import Flex from '@/components/Flex'
 import Container from '@/components/Container/Container'
 import Typography from '@/components/Typography/Typography'
 
-import { SeoEllaIcon } from '@/assets/icons/svgs'
+import { SEOAIRankerIcon } from '@/assets/icons/svgs'
 
 import './RecommendationsSuspense.scss'
 
@@ -10,13 +10,13 @@ const RecommendationSuspense = () => {
   return (
     <Container className="recommendations-suspense-loader" width={100}>
       <Flex align="center" vertical gap={64} className="suspense-loader">
-        <div className="brand-icon auth-Fl" title="SEO Ella icon">
-          {SeoEllaIcon}
+        <div className="brand-icon auth-Fl" title="SEOAiRanker icon">
+          {SEOAIRankerIcon}
         </div>
         <Flex vertical align="center" justify="center" gap={32}>
           <Typography text="Please Wait..." type="h3" />
           <Typography
-            text="SEO Ella is generating recommendations for you, this may take few minutes. We will notify you by email once we are ready."
+            text="SEOAiRanker is generating recommendations for you, this may take few minutes. We will notify you by email once we are ready."
             textAlign="left"
           />
         </Flex>
